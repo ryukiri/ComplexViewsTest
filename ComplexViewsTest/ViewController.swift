1d20c374eb63ece3808de312a3afce833db66ce6
Austin Quach <austinquachh@gmail.com>
2018-02-04 12:12:57 -0800

    Got flip to work

--- ComplexViewsTest/ViewController.swift
+++ ComplexViewsTest/ViewController.swift
@@ -19,7 +19,63 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    fileprivate var firstViewController : FirstViewController!
+    fileprivate var secondViewController : SecondViewController!
+    
+    fileprivate func firstBuilder() {
+        if firstViewController == nil {
+            firstViewController =
+                storyboard?
+                    .instantiateViewController(withIdentifier: "First")
+                as! FirstViewController
+        }
+    }
+    fileprivate func secondBuilder() {
+        if secondViewController == nil {
+            secondViewController =
+                storyboard?
+                    .instantiateViewController(withIdentifier: "Second")
+                as! SecondViewController
+        }
+    }
+    
+    @IBAction func switchViews(_ sender: UIBarButtonItem) {
+        secondBuilder()
+        firstBuilder()
+        
+        UIView.beginAnimations("View Flip", context: nil)
+        UIView.setAnimationDuration(0.4)
+        UIView.setAnimationCurve(.easeInOut)
+        
+        if firstViewController != nil &&
+            firstViewController?.view.superview != nil {
+            UIView.setAnimationTransition(.flipFromRight, for: view, cache: true)
+            secondViewController.view.frame = view.frame
+            switchViewController(firstViewController, to: secondViewController)
+        }
+        else {
+            UIView.setAnimationTransition(.flipFromLeft, for: view, cache: true)
+            firstViewController.view.frame = view.frame
+            switchViewController(secondViewController, to: firstViewController)
+        }
+        UIView.commitAnimations()
 
+    }
+    
+    fileprivate func switchViewController(_ from: UIViewController?, to: UIViewController?) {
+        if from != nil {
+            from!.willMove(toParentViewController: nil)
+            from!.view.removeFromSuperview()
+            from!.removeFromParentViewController()
+        }
+        
+        if to != nil {
+            self.addChildViewController(to!)
+            self.view.insertSubview(to!.view, at: 0)
+            to!.didMove(toParentViewController: self)
+        }
+    }
 
 }
 
